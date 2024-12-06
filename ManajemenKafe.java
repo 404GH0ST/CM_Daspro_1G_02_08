e01da69cb05a3d3e2f03345f9f526f23c970e8d4
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2024-12-06 13:59:21 +0700

    feat: project skeleton

--- ManajemenKafe.java
+++ ManajemenKafe.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class ManajemenKafe {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            System.out.println("===== MENU UTAMA =====");
+            System.out.println("1. Tambahkan Pesanan");
+            System.out.println("2. Tampilkan Daftar Pesanan");
+            System.out.println("3. Keluar");
+            System.out.print("Pilih menu: ");
+        }
+
+        sc.close();
     }
 }
